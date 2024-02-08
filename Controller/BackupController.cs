340b077d47717cdea0f1acdae8da911edc2e1372
BilelAITALLA <bilel.aitalla66@gmail.com>
2024-02-08 16:03:06 +0100

    feat-8 : remontée des informations pour les loggers

--- Controller/BackupController.cs
+++ Controller/BackupController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -46,7 +47,31 @@ namespace Easysave.Controller
 		/// <param name="command"></param>
 		public void ExecuteTasks(string command)
 		{
+			if (command.Contains("-"))
+			{
+				string[] tasks = command.Split('-');
+				int start = int.Parse(tasks[0]);
+				int end = int.Parse(tasks[1]);
 
+				for (int i = start; i <= end; i++)
+				{
+					ExecuteTaskByKey(i);
+				}
+			}
+			else if (command.Contains(";"))
+			{
+				string[] tasks = command.Split(';');
+				foreach (string task in tasks)
+				{
+					int key = int.Parse(task);
+					ExecuteTaskByKey(key);
+				}
+			}
+			else
+			{
+				int key = int.Parse(command);
+				ExecuteTaskByKey(key);
+			}
 		}
 
 		/// <summary>
@@ -72,16 +97,28 @@ namespace Easysave.Controller
 				Directory.CreateDirectory(destinationDirectory); // Create destination directory
 			}
 
-			// Total size of the backup
-			long totalBackupSize = 0;
-
 			// Measure the execution time
 			var stopwatch = Stopwatch.StartNew();
+
+			(long size, int fileCount) = CalculateTransferSize(task);
+
+			// Total size of the backup
+			long totalBackupSize = size;
+
+			// Total number of files to backup
+			int totalFileCount = fileCount;
 
 			// Perform backup (full or differential)
 			Stack<(DirectoryInfo, DirectoryInfo)> stack = new Stack<(DirectoryInfo, DirectoryInfo)>();
 			stack.Push((sourceDir, destDir));
+			
+			// Remaining files and size to backup
+			long filesLeftToDo = totalFileCount;
+			long fileSizeLeftToDo = totalBackupSize;
 
+			// Update the state of the task to active
+			StateTrackLogger.UpdateActive(totalFileCount, totalBackupSize, filesLeftToDo, fileSizeLeftToDo, sourceDirectory, destinationDirectory, key);
+			
 			while (stack.Count > 0)
 			{
 				var (currentSource, currentDestination) = stack.Pop();
@@ -111,7 +148,9 @@ namespace Easysave.Controller
 					if (backupType == BackupType.Full || !destFile.Exists || file.LastWriteTime > destFile.LastWriteTime)
 					{
 						file.CopyTo(targetFilePath, true);
-						totalBackupSize += file.Length;
+						filesLeftToDo--;
+						fileSizeLeftToDo -= file.Length;
+						StateTrackLogger.UpdateActive(totalFileCount, totalBackupSize, filesLeftToDo, fileSizeLeftToDo, Path.Combine(currentSource.FullName, file.Name), targetFilePath, key);
 					}
 				}
 
@@ -124,6 +163,7 @@ namespace Easysave.Controller
 			}
 			stopwatch.Stop();
 			var duration = stopwatch.Elapsed;
+			DailyLogger.WriteDailyLog(task, totalBackupSize, duration.Milliseconds);
 		}
 
 		/// <summary>
@@ -144,5 +184,77 @@ namespace Easysave.Controller
 
 		}
 
+		private (long size, int fileCount) CalculateTransferSize(BackupModel task)
+		{
+			long size = 0;
+			int fileCount = 0;
+			string sourceDirectory = task.SourceDirectory;
+			string destinationDirectory = task.DestinationDirectory;
+			BackupType backupType = task.Type;
+
+			var sourceDir = new DirectoryInfo(sourceDirectory);
+			var destDir = new DirectoryInfo(destinationDirectory);
+
+			if (task.Type == BackupType.Full || !destDir.Exists)
+			{
+				Stack<DirectoryInfo> stack = new Stack<DirectoryInfo>();
+				stack.Push(sourceDir);
+
+				while (stack.Count > 0)
+				{
+					var currentDir = stack.Pop();
+					long dirSize = 0;
+					foreach (FileInfo file in currentDir.GetFiles())
+					{
+						dirSize += file.Length;
+						fileCount++;
+					}
+					size += dirSize;
+
+					foreach (DirectoryInfo subDir in currentDir.GetDirectories())
+					{
+						stack.Push(subDir);
+					}
+				}
+			}
+			else if (task.Type == BackupType.Differential)
+			{
+				Stack<(DirectoryInfo, DirectoryInfo)> stack = new Stack<(DirectoryInfo, DirectoryInfo)>();
+				stack.Push((sourceDir, destDir));
+
+				while (stack.Count > 0)
+				{
+					var (currentSource, currentDestination) = stack.Pop();
+
+					var dir = currentSource;
+
+					if (!dir.Exists)
+						throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+					DirectoryInfo[] dirs = dir.GetDirectories();
+
+					foreach (FileInfo file in dir.GetFiles())
+					{
+						string targetFilePath = Path.Combine(currentDestination.FullName, file.Name);
+						FileInfo destFile = new FileInfo(targetFilePath);
+
+						if (!destFile.Exists || file.LastWriteTime > destFile.LastWriteTime)
+						{
+							size += file.Length;
+							fileCount++;
+						}
+					}
+
+					// Add subdirectories to the stack for further processing
+					foreach (DirectoryInfo subDir in dirs)
+					{
+						string newDestinationDir = Path.Combine(currentDestination.FullName, subDir.Name);
+						stack.Push((subDir, new DirectoryInfo(newDestinationDir)));
+					}
+				}
+			}
+
+			return (size, fileCount);
+		}
 	}
 }
