270b74cd1be85d000b3652d200887a854d33b760
BilelAITALLA <bilel.aitalla66@gmail.com>
2024-02-24 16:12:25 +0100

    publish branch + fix packages

--- Model/AppConfigData.cs
+++ Model/AppConfigData.cs
@@ -3,7 +3,7 @@ using System;
 using System.IO;
 using System.Text.Json;
 
-namespace Easysave.Model
+namespace easysave.Model
 {
     public class AppConfigData
     {
--- Model/Config.cs
+++ Model/Config.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 using Newtonsoft.Json;
 
-namespace Easysave.Model
+namespace easysave.Model
 {
 	public class Config
 	{
