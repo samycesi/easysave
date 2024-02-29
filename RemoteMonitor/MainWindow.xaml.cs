1d17da4d19ef3f46421f8382421a963b59d8201a
samycesi <samy.amal@viacesi.fr>
2024-02-29 23:10:48 +0100

    feat-20_remote-monitor-restructuration

---
+++ RemoteMonitor/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using RemoteMonitor.ViewModel;
+using System.Windows;
+
+namespace RemoteMonitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = new MainViewModel();
+            DataContext = viewModel;
+        }
+    }
+}
